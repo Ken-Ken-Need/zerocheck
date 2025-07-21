26fbdd997f276e0b04f9770847c7a1b2a7607b48
KenKenNeed <2317150243@qq.com>
2025-07-21 19:09:50 -0400

    test command

--- bin/test_unchecked_kzg.sh
+++ bin/test_unchecked_kzg.sh
@@ -11,7 +11,7 @@ REPEAT=1
 MIN_SIZE=8
 MAX_SIZE=8
 
-CMDS1="cargo run --release --bin univar_opt_bench_multhr -- --repeat=1 --min-size=8 --max-size=8 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=msm_unchecked_kzg --batch-opening-threads=8 | tee -a output_log/univar_opt_bench_run_1_open_8.log "
+CMDS1="cargo run --release --bin univar_opt_bench_multhr -- --repeat=1 --min-size=8 --max-size=8 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=msm_unchecked_kzg --batch-opening-threads=8 | tee -a output_log/new.log "
 
 
 echo "Running: $CMDS1"
